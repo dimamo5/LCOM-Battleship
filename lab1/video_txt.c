65fc06244d18c4230203a0389016249e1ce3f610
up201304068 <up201304068@d0764612-f3d4-4dc5-83ea-28fe56a98991>
2014-09-29 16:34:54 +0000

    Versao final aula

--- lab1/video_txt.c
+++ lab1/video_txt.c
@@ -20,12 +20,12 @@ void vt_fill(char ch, char attr) {
 	vptr=video_mem;
 	int i =0;
 	for(i;i<scr_width*scr_lines;i++,vptr++){
-			*vptr=ch;
-			vptr++;
-			*vptr=attr;
-		}
+		*vptr=ch;
+		vptr++;
+		*vptr=attr;
+	}
 }
-  
+
 
 void vt_blank() {
 	vt_fill(0x00,0x00);
@@ -34,11 +34,11 @@ void vt_blank() {
 
 int vt_print_char(char ch, char attr, int r, int c) {
 	char *vptr;
-	if(r>=scr_lines || c>=scr_width){
-		return 1;
+	if(r>=scr_lines || c>=scr_width || r<0||c<0){
+		return -1;
 	}
 	vptr=video_mem;
-	vptr=vptr+2*scr_width*r+2*c; // 2*((r+1)*scr_width - (scr_width-c));
+	vptr=vptr+2*scr_width*r+2*c;
 	*vptr=ch;
 	vptr++;
 	*vptr=attr;
@@ -47,6 +47,12 @@ int vt_print_char(char ch, char attr, int r, int c) {
 }
 
 int vt_print_string(char *str, char attr, int r, int c) {
+	if(r>=scr_lines || c>=scr_width || r<0||c<0){
+		return -1;
+	}
+	if(80-c<strlen(str)){
+		return -1;
+	}
 	int lim=strlen(str);
 	int i =0;
 	char *vptr;
@@ -57,31 +63,27 @@ int vt_print_string(char *str, char attr, int r, int c) {
 		vptr++;
 		*vptr=attr;
 	}
+
 	return 0;
 
 }
 
 int vt_print_int(int num, char attr, int r, int c) {
+	if(r>=scr_lines || c>=scr_width || r<0||c<0){
+		return -1;
+	}
 	int i =0;
 	char *vptr;
 	vptr=video_mem;
 	vptr=vptr+2*scr_width*r+2*c;
 	int num_temp=num;
 	int cont =0;
-	while(num_temp>0){
-	num_temp=num_temp/10;
+	do{num_temp=num_temp/10;
 	cont++;
-	}
-	/*cont--;
-	for(i;i<=cont;i++,vptr++,cont--){
-		int num_temp2=num;
-		*vptr=num_temp2/pow(10,cont)+0x30;
-		vptr++;
-		*vptr=attr;
-	}*/
+	}while(num_temp>0);
+
 	vptr=vptr+2*(cont-1);
 	for(i;i<cont;i++){
-		//*vptr=num_temp2%(int)pow(10,i))/(pow(10,(i-1)));
 		*vptr=(num%10)+0x30;
 		num=num/10;
 		vptr++;
@@ -94,9 +96,40 @@ int vt_print_int(int num, char attr, int r, int c) {
 
 
 int vt_draw_frame(int width, int height, char attr, int r, int c) {
-
-  /* To complete ... */
-
+	if(r>=scr_lines || c>=scr_width || r<0||c<0){
+		return -1;
+	}
+	if(r+height>25||c+width>80){
+		return -1;
+	}
+	char *vptr;
+	vptr=video_mem;
+	vptr=vptr+2*scr_width*r+2*c;
+	vptr++;
+	int i =0;
+	for(i;i<width;i++){
+		*vptr=attr;
+		vptr=vptr+2;
+	}
+	i=0;
+	vptr= vptr-2;
+	for(i;i<height;i++){
+		*vptr=attr;
+		vptr=vptr+80*2;
+	}
+	i=0;
+	vptr=vptr-(80*2)-2;
+	for(i;i<width-1;i++){
+		*vptr=attr;
+		vptr=vptr-2;
+	}
+	i=0;
+	vptr=vptr+2;
+	for(i;i<height-1;i++){
+		*vptr=attr;
+		vptr=vptr-(80*2);
+	}
+	return 0;
 }
 
 /*
@@ -105,28 +138,28 @@ int vt_draw_frame(int width, int height, char attr, int r, int c) {
 
 char *vt_init(vt_info_t *vi_p) {
 
-  int r;
-  struct mem_range mr;
+	int r;
+	struct mem_range mr;
 
-  /* Allow memory mapping */
+	/* Allow memory mapping */
 
-  mr.mr_base = (phys_bytes)(vi_p->vram_base);
-  mr.mr_limit = mr.mr_base + vi_p->vram_size;
+	mr.mr_base = (phys_bytes)(vi_p->vram_base);
+	mr.mr_limit = mr.mr_base + vi_p->vram_size;
 
-  if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
-	  panic("video_txt: sys_privctl (ADD_MEM) failed: %d\n", r);
+	if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
+		panic("video_txt: sys_privctl (ADD_MEM) failed: %d\n", r);
 
-  /* Map memory */
+	/* Map memory */
 
-  video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vi_p->vram_size);
+	video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vi_p->vram_size);
 
-  if(video_mem == MAP_FAILED)
-	  panic("video_txt couldn't map video memory");
+	if(video_mem == MAP_FAILED)
+		panic("video_txt couldn't map video memory");
 
-  /* Save text mode resolution */
+	/* Save text mode resolution */
 
-  scr_lines = vi_p->scr_lines;
-  scr_width = vi_p->scr_width;
+	scr_lines = vi_p->scr_lines;
+	scr_width = vi_p->scr_width;
 
-  return video_mem;
+	return video_mem;
 }
