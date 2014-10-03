f1b9ec9e348722e0383869ee043367671bb0982d
Diogo Moura <diogomoura13@gmail.com>
2014-10-03 16:02:38 +0000

    Fix lab2.c and start developing timer_get_conf

---
+++ lab2/lab2.c
@@ -0,0 +1,6 @@
+#include <minix/drivers.h>
+#include "timer.h"
+
+int main(int argc, char **argv) {
+
+}
--- lab2/timer.c
+++ lab2/timer.c
@@ -1,19 +1,21 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
+#include <timer.h>
+#include <i8254.h>
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
-	return 1;
+	return 0;
 }
 
 int timer_subscribe_int(void ) {
 
-	return 1;
+	return 0;
 }
 
 int timer_unsubscribe_int() {
 
-	return 1;
+	return 0;
 }
 
 void timer_int_handler() {
@@ -21,26 +23,37 @@ void timer_int_handler() {
 }
 
 int timer_get_conf(unsigned long timer, unsigned char *st) {
+	if(timer<0||timer>2){
+			return -1;
+		}
+	unsigned char temp=0xC0; //Initialize ReadBack Command
+	temp |= TIMER_RB_SEL(timer) | TIMER_RB_STATUS_|TIMER_RB_COUNT_; // Read Back Command
+	*st=temp; //execute previous command
 	
-	return 1;
+	return 0;
 }
 
 int timer_display_conf(unsigned char conf) {
+	unsigned char temp=*(0x43);
 	
-	return 1;
+	return 0;
 }
 
 int timer_test_square(unsigned long freq) {
 	
-	return 1;
+	return 0;
 }
 
 int timer_test_int(unsigned long time) {
 	
-	return 1;
+	return 0;
 }
 
 int timer_test_config(unsigned long timer) {
-	
-	return 1;
+	if(timer<0||timer>2){
+		return -1;
+	}
+	//int timer_get_conf(unsigned char timer, unsigned char *st)
+	//int timer_display_conf(unsigned char conf)
+	return 0;
 }
