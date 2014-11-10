d9857dfc2c66df746278689c69ba47c609ec116a
Diogo Moura <diogomoura13@gmail.com>
2014-11-10 14:05:03 +0000

    Funcao 3 a funcionar e 4 a meio

--- lab4/test4.c
+++ lab4/test4.c
@@ -1,18 +1,18 @@
 #include "test4.h"
 #include "mouse.h"
 
-int test_packet(unsigned short cnt){
+int test_packet(unsigned short cnt) {
 	return mouse_packet(cnt);
-}	
-	
-int test_async(unsigned short idle_time) {
-	return mouse_packet_async (idle_time);
 }
-	
+
+int test_async(unsigned short idle_time) {
+	return mouse_packet_async(idle_time);
+}
+
 int test_config(void) {
-    /* To be completed ... */
-}	
-	
+	return config();
+}
+
 int test_gesture(short length, unsigned short tolerance) {
-    /* To be completed ... */
+	/* To be completed ... */
 }
