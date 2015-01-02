d30ecce102e9f480b7cafd73e1a5027d3f3c7c86
Diogo Moura <diogomoura13@gmail.com>
2015-01-02 02:15:04 +0000

    highscore quase feito

---
+++ proj/src/highscore.h
@@ -0,0 +1,49 @@
+#ifndef _HIGHSCORE_H_
+#define _HIGHSCORE_H_
+
+#include "graphics.h"
+#include "battleship.h"
+#include "game.h"
+#include "bitmap.h"
+#include "keyboard_mouse.h"
+
+#define highscore ((Highscore_State*) battle->state)
+
+typedef struct {
+	char nome[20];
+	char data[20];
+	int score;
+} Jogador;
+
+typedef struct {
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
+	unsigned int hour;
+	unsigned int min;
+
+	Jogador jogador_array[5]; //array ordenado (decrescente) de scores
+
+	unsigned short show; //0 esta a escrever //1 esta a mostrar
+
+	char* nome_player;
+
+	unsigned short score_introduzir;
+	unsigned short nome_introduzido;
+	unsigned short done;
+
+	Bitmap* fonts;
+
+} Highscore_State;
+
+Highscore_State* newHighscore(Battleship* battle);
+
+void drawHighscore(Battleship* battle);
+State updateHighscore(Battleship* battle);
+void deleteHighscore(Battleship* battle);
+char getKey(Battleship* battle);
+void loadScores(Highscore_State* state);
+void string_conc(char* str1, char ch);
+char* getStringJogador(Battleship* battle, unsigned int i);
+int entrouHighscore(Battleship* battle);
+#endif
